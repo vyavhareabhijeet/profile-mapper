57fef21a5b224f8fb20dc087759917f463f17fdd
202202040012_Abhijeet _Vyavhare <abhijeetvyavahare2003@gmail.com>
2024-04-22 07:55:58 +0530

    second commit

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,4 +1,4 @@
-// Profile.js
+
 import React from 'react';
 import ProfileDetail from './ProfileDetail';
 import './Profile.css';
@@ -9,10 +9,19 @@ const Profile = ({ profile }) => {
   const handleSummaryClick = () => {
     const popupWindow = window.open('', '_blank', 'width=600,height=600');
     popupWindow.document.title = `Profile - ${profile.name}`;
-    popupWindow.document.body.innerHTML = `<div id="profile-detail-container"></div>`;
-    const profileDetailContainer = popupWindow.document.getElementById('profile-detail-container');
+    
+    // Check if the container element exists, if not, create it
+    let profileDetailContainer = popupWindow.document.getElementById('profile-detail-container');
+    if (!profileDetailContainer) {
+      profileDetailContainer = popupWindow.document.createElement('div');
+      profileDetailContainer.id = 'profile-detail-container';
+      popupWindow.document.body.appendChild(profileDetailContainer);
+    }
+    
+    // Render the ProfileDetail component into the container
     ReactDOM.render(<ProfileDetail profile={profile} />, profileDetailContainer);
   };
+  
 
   return (
     <div className="profile">
